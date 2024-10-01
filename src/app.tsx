f1cf203329b0dd0bafc071a3fa646fe7efea9c05
Brenley Dueck <brenleydueck@gmail.com>
2024-10-01 12:41:43 -0500

    Setup SolidUi

--- src/app.tsx
+++ src/app.tsx
@@ -3,15 +3,14 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import "@fontsource/inter"
 
 export default function App() {
   return (
     <Router
       root={props => (
         <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
+          <Title>Solid Issue Tracker Lite - Brenelz</Title>
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
