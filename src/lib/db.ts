70700a0a7a560a04c62bf27b202c74eaaf5faa01
Brenley Dueck <brenleydueck@gmail.com>
2024-10-01 21:10:47 -0500

    Setup clerk webhooks

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,14 +4,37 @@ import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 
 const client = createClient({
-    url: process.env.TURSO_CONNECTION_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+  url: process.env.TURSO_CONNECTION_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
 export const db = drizzle(client);
 
 export const usersTable = sqliteTable('users', {
   id: text('id'),
-  textModifiers: text('text_modifiers').notNull().default(sql`CURRENT_TIMESTAMP`),
-  intModifiers: integer('int_modifiers', { mode: 'boolean' }).notNull().default(false),
+  username: text('username'),
+  email: text('email'),
+  avatar: text('avatar'),
+  firstName: text('firstName'),
+  lastName: text('lastName'),
+});
+
+export const issuesTable = sqliteTable('issues', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  ownerId: text('ownerId').notNull().references(() => usersTable.id),
+  assignedId: text('assignedId').references(() => usersTable.id),
+  parentIssueId: integer('parentIssueId'),
+  title: text('title'),
+  description: text('description'),
+  stacktrace: text('stacktrace'),
+  createdAt: text("timestamp").default(sql`(CURRENT_TIMESTAMP)`),
+  resolvedAt: text("timestamp")
+});
+
+export const notificationsTable = sqliteTable('notifications', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  userId: text('userId').notNull().references(() => usersTable.id),
+  issueId: text('issueId').references(() => issuesTable.id),
+  createdAt: text("timestamp").default(sql`(CURRENT_TIMESTAMP)`),
+  readAt: text("timestamp")
 });
---
+++ src/routes/api/clerk.ts
@@ -0,0 +1,69 @@
+import { json } from "@solidjs/router";
+import type { APIEvent } from "@solidjs/start/server";
+import { Webhook } from 'svix';
+import { readRawBody } from "vinxi/http";
+import { db, usersTable } from "~/lib/db";
+
+const handler = async (event: APIEvent) => {
+    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+    if (!WEBHOOK_SECRET) {
+        throw new Error('You need a WEBHOOK_SECRET in your .env')
+    }
+
+    // Get the headers and body
+    const headers = event.request.headers
+    const payload = await readRawBody(event.nativeEvent);
+
+    // Get the Svix headers for verification
+    const svix_id = headers.get('svix-id');
+    const svix_timestamp = headers.get('svix-timestamp');
+    const svix_signature = headers.get('svix-signature');
+
+    // If there are no Svix headers, error out
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+        return new Response('Error occured -- no svix headers', {
+            status: 400,
+        })
+    }
+
+    // Create a new Svix instance with your secret.
+    const wh = new Webhook(WEBHOOK_SECRET)
+
+    let evt: any
+
+    // Attempt to verify the incoming webhook
+    // If successful, the payload will be available from 'evt'
+    // If the verification fails, error out and  return error code
+    try {
+        evt = wh.verify(payload!, {
+            'svix-id': svix_id,
+            'svix-timestamp': svix_timestamp,
+            'svix-signature': svix_signature,
+        })
+    } catch (err) {
+        const error: any = err;
+        console.log('Error verifying webhook:', error.message)
+        return new Response(error.message);
+    }
+
+    // Do something with the payload
+    // For this guide, you simply log the payload to the console
+    const { id } = evt.data
+    const eventType = evt.type
+
+    if (eventType === 'user.created') {
+        await db.insert(usersTable).values({
+            id: evt.data.id,
+            username: evt.data.username,
+            email: evt.data.email_addresses[0].email_address,
+            avatar: evt.data.profile_image_url,
+            firstName: evt.data.first_name,
+            lastName: evt.data.last_name,
+        })
+    }
+
+    return json({ success: true });
+
+}
+export const GET = handler;
+export const POST = handler;
