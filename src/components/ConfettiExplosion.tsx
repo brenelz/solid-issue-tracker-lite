f76a5788edcb309ec78cdd41f450ab8c790f31c5
Brenley Dueck <brenleydueck@gmail.com>
2024-10-15 17:44:20 -0500

    Show confetti with no unresolved issues

---
+++ src/components/ConfettiExplosion.tsx
@@ -0,0 +1,8 @@
+
+import { ConfettiExplosion } from 'solid-confetti-explosion';
+
+export default function ConfettiExplosionWrapper() {
+    return (
+        <ConfettiExplosion />
+    )
+}
--- src/components/IssueTabs.tsx
+++ src/components/IssueTabs.tsx
@@ -3,6 +3,12 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs"
 import IssuesList from "./IssuesList";
 import { IssueWithAssignedUser } from "~/lib/data";
+import { clientOnly } from '@solidjs/start';
+import { createEffect, Show } from "solid-js";
+import { useSubmission } from "@solidjs/router";
+import { resolveIssues } from "~/lib/actions";
+
+const ConfettiExplosion = clientOnly(() => import('../components/ConfettiExplosion'));
 
 type IssueTabsProps = {
     issues: {
@@ -13,6 +19,8 @@ type IssueTabsProps = {
 }
 
 export default function IssueTabs(props: IssueTabsProps) {
+    const sub = useSubmission(resolveIssues);
+
     return (
         <Tabs defaultValue="unresolved" >
             <TabsList class="grid w-full grid-cols-2" >
@@ -20,6 +28,9 @@ export default function IssueTabs(props: IssueTabsProps) {
                 <TabsTrigger value="resolved">Resolved</TabsTrigger>
             </TabsList>
             <TabsContent value="unresolved">
+                <Show when={sub.input && props.issues?.unresolved.length === 0}>
+                    <ConfettiExplosion />
+                </Show>
                 <IssuesList onDateFilterChange={props.onDateFilterChange} issues={props.issues?.unresolved} type="unresolved" />
             </TabsContent>
             <TabsContent value="resolved">
