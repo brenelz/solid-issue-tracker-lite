102f9dbd8d4c49b99c98c4baec1b9b284d18e4a6
Brenley Dueck <brenleydueck@gmail.com>
2024-11-02 19:18:18 -0500

    Add security (#3)
    * Add security
    
    * Only use csrf for now

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,13 @@
 import { createMiddleware } from '@solidjs/start/middleware';
 import { clerkMiddleware } from 'clerk-solidjs/start/server';
+import { csrfProtection } from "shieldwall/start";
 
 export default createMiddleware({
     onRequest: [
         clerkMiddleware({
             publishableKey: process.env.VITE_CLERK_PUBLISHABLE_KEY,
             secretKey: process.env.CLERK_SECRET_KEY
-        })
+        }),
+        csrfProtection,
     ]
 });
