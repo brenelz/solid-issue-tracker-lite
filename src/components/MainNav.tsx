70a9d2e7f4a365a4ad3b9789c4ab42309911b7db
Brenley Dueck <brenleydueck@gmail.com>
2024-11-02 19:11:57 -0500

    Fix main nav active class

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -11,10 +11,10 @@ export function MainNav(props: ComponentProps<"nav">) {
     const [, rest] = splitProps(props, ["class"])
     return (
         <nav class={cn("flex items-center space-x-4 lg:space-x-6", props.class)} {...rest}>
-            <a href={Routes().dashboard.index} class="text-sm font-medium transition-colors hover:text-white text-secondary" classList={{ "!text-white border-b leading-7": location.pathname === '/dashboard/' }}>
+            <a href={Routes().dashboard.index} class="text-sm font-medium transition-colors hover:text-white text-secondary" classList={{ "!text-white border-b leading-7": location.pathname === '/dashboard' }}>
                 Dashboard
             </a>
-            <a href={Routes().dashboard.issues.index} classList={{ "!text-white border-b leading-7": location.pathname === '/dashboard/issues' }}>
+            <a href={Routes().dashboard.issues.index} class="text-sm font-medium transition-colors hover:text-white text-secondary" classList={{ "!text-white border-b leading-7": location.pathname === '/dashboard/issues' }}>
                 Issues
             </a>
             <a href="https://github.com/brenelz/solid-issue-tracker-lite" target="_blank" rel="noopener" class="text-sm font-medium transition-colors hover:text-white text-secondary">
